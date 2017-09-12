f3c4f6fa7d6aabc794c6fcfa08a07bf7495e133c
jean-philippe-bousselin <jp.bousselin@gmail.com>
2017-09-12 04:07:21 +0200

    Added missing file

---
+++ app/services/TeamService.scala
@@ -0,0 +1,20 @@
+package services
+
+import javax.inject.Inject
+
+import db._
+import models._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class TeamService @Inject()(
+  teamDao: TeamDao
+) {
+
+  def add(team: Team) : Future[Team] = {
+    teamDao.add(team)
+  }
+
+
+}
